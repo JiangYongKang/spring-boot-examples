0a7d24202ae1570e14de6e47fa150ef7b05b87fd
vincent <vincent4502237@gmail.com>
2017-07-01 22:15:54 +0800

    配置文件的灵活使用

---
+++ chapter-02/src/test/java/com/vincent/PropertiesConfigTest.java
@@ -0,0 +1,25 @@
+package com.vincent;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PropertiesConfigTest {
+
+    @Resource
+    private PropertiesConfig propertiesConfig;
+
+    @Test
+    public void contextLoads() {
+        Assert.assertEquals("vincent", propertiesConfig.getName());
+        Assert.assertEquals(25L, propertiesConfig.getAge());
+        Assert.assertEquals("vincent, 25", propertiesConfig.getDesc());
+    }
+
+}
