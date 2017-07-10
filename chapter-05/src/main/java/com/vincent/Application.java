6f79b4f19afe11a0fac8f134a371a4e0220e3ff9
JiangYongKang <vincent4502237@gmail.com>
2017-07-10 18:14:03 +0800

    springboot 使用 Swagger2 构建强大的 RESTful APIs 文档

---
+++ chapter-05/src/main/java/com/vincent/Application.java
@@ -0,0 +1,42 @@
+package com.vincent;
+
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    @ApiOperation(value = "返回两个操作数的乘积", notes = "这里是接口的详细说明")
+    /*@ApiImplicitParams({
+            @ApiImplicitParam(name = "operand1", value = "操作数一", required = true, dataType = "Double", paramType = "String"),
+            @ApiImplicitParam(name = "operand2", value = "操作数二", required = true, dataType = "Double")
+    })*/
+    @RequestMapping(value = "/addition/{operand1}/{operand2}", method = RequestMethod.POST)
+    public Double index(@PathVariable(value = "operand1") Double operand1,
+                        @PathVariable(value = "operand2") Double operand2,
+                        @RequestBody Map<String, Character> operator) {
+        switch (operator.get("operator")) {
+            case '+':
+                return operand1 + operand2;
+            case '-':
+                return operand1 + operand2;
+            case '*':
+                return operand1 * operand2;
+            case '/':
+                return operand1 / operand2;
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ chapter-05/src/main/java/com/vincent/Swagger2Config.java
@@ -0,0 +1,43 @@
+package com.vincent;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by IDEA.
+ * User: e
+ * Date: 2017/7/10
+ * Comment: Swagger2 的配置类
+ * @Configuration 让 Spring 来加载该类配置
+ * @EnableSwagger2 启用 Swagger2
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2Config {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.vincent"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo () {
+        return new ApiInfoBuilder()
+                .title("Spring Boot 中使用 Swagger2 构建 RESTful APIs ")
+                .description("接口的简单说明")
+                .termsOfServiceUrl("接口说明地址")
+                .contact("这里写上作者的大名")
+                .version("这里写版本号")
+                .build();
+    }
+}
