4731f5ff3f460c7acdba7770f5c247de97ee8ec5
JiangYongKang <vincent4502237@gmail.com>
2017-07-11 10:31:53 +0800

    springboot 统一全局异常处理

---
+++ chapter-06/src/main/java/com/vincent/Application.java
@@ -0,0 +1,25 @@
+package com.vincent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@Controller
+public class Application {
+
+    @RequestMapping("/global/error")
+    public String error() throws Exception {
+        throw new Exception("这是一个测试异常消息");
+    }
+
+    @RequestMapping("/global/error/json")
+    public String json() throws GlobalException {
+        throw new GlobalException("发生错误");
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
