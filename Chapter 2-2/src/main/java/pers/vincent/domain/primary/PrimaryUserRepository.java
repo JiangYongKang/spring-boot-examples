a7fa4c28e11f040a41e4bafe4687f99f1061d08a
JiangYongKang <vincent4502237@gmail.com>
2017-07-26 16:17:47 +0800

    Spring Boot多数据源配置与使用

---
+++ Chapter 2-2/src/main/java/pers/vincent/domain/primary/PrimaryUserRepository.java
@@ -0,0 +1,6 @@
+package pers.vincent.domain.primary;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PrimaryUserRepository extends JpaRepository<PrimaryUserInfo, Long> {
+}
---
+++ Chapter 2-2/src/main/java/pers/vincent/domain/secondary/SecondaryUserRepository.java
@@ -0,0 +1,6 @@
+package pers.vincent.domain.secondary;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SecondaryUserRepository extends JpaRepository<SecondaryUserInfo, Long> {
+}
