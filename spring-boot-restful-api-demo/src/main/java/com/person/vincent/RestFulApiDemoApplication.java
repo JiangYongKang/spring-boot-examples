929060b2aa3b00a5ff234abb573b3a259e6a5320
vincent <vincent4502237@gmail.com>
2017-11-29 10:26:52 +0800

    spring boot restful api demo

--- spring-boot-restful-api-demo/src/main/java/com/person/vincent/RestFulApiDemoApplication.java
+++ spring-boot-restful-api-demo/src/main/java/com/person/vincent/RestFulApiDemoApplication.java
@@ -1,12 +1,12 @@
-package com.vincent;
+package com.person.vincent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class RestFulApiDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(RestFulApiDemoApplication.class, args);
     }
 }
--- spring-boot-restful-api-demo/src/test/java/com/person/vincent/controller/UserControllerTest.java
+++ spring-boot-restful-api-demo/src/test/java/com/person/vincent/controller/UserControllerTest.java
@@ -1,21 +1,19 @@
-package com.vincent;
+package com.person.vincent.controller;
 
+import com.person.vincent.UserController;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.UUID;
 
 import static org.hamcrest.Matchers.equalTo;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
