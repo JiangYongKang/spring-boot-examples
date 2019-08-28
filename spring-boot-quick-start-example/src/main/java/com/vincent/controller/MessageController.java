1ceaec96c4de08207880fbead39f9ed58cf30001
vincent <vincent4502237@gmail.com>
2019-08-28 22:42:09 +0800

    更新版本

---
+++ spring-boot-quick-start-example/src/main/java/com/vincent/controller/MessageController.java
@@ -0,0 +1,24 @@
+package com.vincent.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Author: vincent
+ * Date: 2019/8/28 10:30 下午
+ * Comment:
+ */
+
+@RestController
+@RequestMapping("/message")
+public class MessageController {
+
+    @GetMapping
+    public ResponseEntity<?> index(@RequestParam("name") String name) {
+        return ResponseEntity.ok("Hello " + name);
+    }
+
+}
