199abcbe89f16118fa35c5d9caf2726939447e4e
vincent <vincent4502237@gmail.com>
2019-08-31 17:14:24 +0800

    在 springboot 中使用 thymeleaf

---
+++ spring-boot-thymeleaf-example/src/main/java/com/vincent/example/ThymeleafExampleApplication.java
@@ -0,0 +1,11 @@
+package com.vincent.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafExampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafExampleApplication.class, args);
+    }
+}
